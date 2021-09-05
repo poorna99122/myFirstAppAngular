f32ccdb853baacb7b4f49aaa1877479ea2b9633a
poorna99122 <poornachandrarao99122@gmail.com>
2021-09-04 21:49:58 -0700

    added recipe list with hyper link

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes : Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2015/11/shakshuka-11.jpg')
+  ];
 
   constructor() { }
 
